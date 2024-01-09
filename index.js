c80273248daf1a50ab9decaeefd3841e75539123
Detroja-Jenish <114487290+Detroja-Jenish@users.noreply.github.com>
2024-01-09 18:25:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,31 +14,20 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'imgs'))) 
 app.use(fileUpload())
 app.post("/send", (req,res)=>{
-    console.log(req)
-    console.log("==========================")
-    console.log("==========================")
-    console.log("==========================")
-    console.log("==========================")
-    console.log("==========================")
-    console.log("==========================")
-    console.log("==========================")
-    // console.log(req.query)
-    // console.log(req.body.name)
-    // console.log(req.body.caption)
-    // console.log(req.files)
-    // savedImgPath = req.files.myFile.name.replaceAll(' ','')
-    data.push({fullName: req.body.fullName,
-        enroll: req.body.enroll,
-        sem: req.body.sem,
-        email: req.body.email,
-        favSports: req.body.favSports,
-        address: req.body.address,
-        gender: req.body.gender,
-        city: req.body.city,
-        img: 'http://127.0.0.1:3000/'+req.files.img.name.replaceAll(' ','')
-    })
     
-    req.files.img.mv('./imgs/'+req.files.img.name.replaceAll(" ",''))
+    data.push({fullName: req.body.fullName,
+        enroll: (enroll in req.body) ? req.body.enroll : "",
+        sem: (sem in req.body) ? req.body.sem : "",
+        email: (email in req.body) ? req.body.email : "",
+        favSports: (favSports in req.body) ? req.body.favSports : "",
+        address: (address in req.body) ? req.body.address : "",
+        gender: (gender in req.body) ? req.body.gender : "",
+        city: (city in req.body) ? req.body.city : "",
+        img:(img in req.files) ?  'http://127.0.0.1:3000/'+req.files.img.name.replaceAll(' ','') : ""
+    })
+    if(img in req.files){
+        req.files.img.mv('./imgs/'+req.files.img.name.replaceAll(" ",''))
+    }
     res.send(req.body);
 })
 
@@ -50,21 +39,14 @@ app.get("/get", (req,res)=>{
 app.get("/clearAll" , (req,res)=>{
     data = [];
     files = fs.readdirSync('./imgs')
-   
-    // fs.unlink("./imgs/*",()=>{console.log("shbdfs===")})
-    console.log(files)
+
     for(file of files){
         if(file != "abc.txt"){
             fs.unlink("./imgs/"+file,()=>{console.log("shbdfs===")})
         }
     }
-    fs.mkdir(path.join(__dirname, 'imgs'),(err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log('Directory created successfully!'); 
-    })
-    fs.rmdir("./imgs", ()=>{console.log("shbd")})
+
+
     res.send("all cleared")
 })
 
