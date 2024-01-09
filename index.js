e234f7e504e01f3503543b772dfb288b696f8e7a
Detroja-Jenish <114487290+Detroja-Jenish@users.noreply.github.com>
2024-01-09 19:32:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/send", (req,res)=>{
     if(req.files.img !=null && req.files.img != undefined){
         req.files.img.mv('./imgs/'+req.files.img.name.replaceAll(" ",''))
     }
-    res.status(202);
+    res.status(202).send('data saved');
 })
 
 app.get("/get", (req,res)=>{
